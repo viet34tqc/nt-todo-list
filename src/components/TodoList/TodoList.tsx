5cef383d2d6220d85fc6c71ca15c1c649160cace
viet34tqc <viet34tqc@gmail.com>
2021-10-15 00:22:12 +0700

    Move todos fetching into TodoList component

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from 'react';
+import { API_URL } from '../../config/config';
 import { useTodos } from '../../context/TodoContext';
+import { Todo } from '../../models/todo';
 import TodoItem from '../TodoItem/TodoItem';
 import './TodoList.styles.scss';
 
 const TodoList = () => {
-	const { todos } = useTodos();
+	const { todos, setTodos } = useTodos();
+	const [error, setError] = useState('');
+
+	// Call API to get Todos
+	useEffect(() => {
+		(async function () {
+			await fetch(API_URL)
+				.then(async (res) => {
+					const todos: Todo[] = await res.json();
+					setTodos(() => todos);
+				})
+				.catch((error) => {
+					setError(error.message);
+				});
+		})();
+	}, [setTodos]);
+
+	console.log( todos );
+
 	if (!todos.length) {
 		return <p>Loading...</p>;
+	} else if (error) {
+		return <p>{error}</p>;
 	}
 	return (
 		<ul className="todo-list">
